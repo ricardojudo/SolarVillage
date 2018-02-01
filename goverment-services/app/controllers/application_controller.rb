f04eec07a058886ab4cb9c8f04d364edb74e98ed
Ricardo Castañeda <ricardojudo@gmail.com>
2018-02-01 12:05:35 -0600

    Header Content-Type to application/json

--- goverment-services/app/controllers/application_controller.rb
+++ goverment-services/app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :error_record_not_found_render_method
   #respond_to :json
 
-
+  before_action :set_headers
+  
+    
 
     private
     def error_record_not_found_render_method(_e)
         render json: { 'statusCode' => 404, 'error' => 'Resource Not Found' }, status: 404
+    end
+
+    def set_headers
+        response.headers['Content-Type'] = 'application/json'
     end
 
 end
