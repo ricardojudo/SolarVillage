026249408d13369c96cdb3fa52536a53698c54b4
ricardojudo <ricardojudo@gmail.com>
2018-02-08 15:59:47 -0500

    Querying process variables

--- new-order-client-scripts/curl/list-new-orders.sh
+++ new-order-client-scripts/curl/list-new-orders.sh
@@ -3,7 +3,7 @@
 CONTAINER_ID=solar-village
 PROCESS_DEFINITION=new-order-permitting-kjar.NewOrderProcess
 #URL=http://localhost:8080/kie-server/services/rest/server/queries/containers/${CONTAINER_ID}/process/instances
-URL=http://localhost:8080/kie-server/services/rest/server/queries/processes/${PROCESS_DEFINITION}/instances
+URL=http://localhost:8080/kie-server/services/rest/server/queries/processes/${PROCESS_DEFINITION}/instances?status=$1
 
 #Status 1=IN_PROGRESS 2=COMPLETED 3=ABORTED
 
