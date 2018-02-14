5a2e2d4851a362adbb23e9091e11832b1c918c24
ricardojudo <ricardojudo@gmail.com>
2018-02-14 12:57:00 -0500

    Refactoring scripts

--- new-order-client-scripts/delete-new-order.sh
+++ new-order-client-scripts/delete-new-order.sh
@@ -5,6 +5,6 @@ source ./set-env.sh
 URL=${KIE_SERVER_HOST}/kie-server/services/rest/server/containers/${KIE_CONTAINER_ID}/processes/instances/$1
 
 echo "Process instance: $1"
-CURL="curl -X DELETE -H \"Accept: application/json\" --user jboss:bpms ${URL}"
+CURL="curl -X DELETE -H \"Accept: application/json\" --user ${CREDENTIALS} ${URL}"
 #echo ${CURL}
 echo ${CURL} | bash
--- new-order-client-scripts/post-new-order.sh
+++ new-order-client-scripts/post-new-order.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source ./set-env.sh
 
 CONTAINER=${KIE_SERVER_HOST}/kie-server/services/rest/server/containers/${KIE_CONTAINER_ID}
@@ -8,3 +9,20 @@ DATA="'{\"address\":\"$1\", \"condo\":\"$2\", \"hoaMeetingDate\":\"$3\"}'"
 CURL="curl -X POST -H \"Accept: application/json\" -H \"Content-Type: application/json\" --user ${CREDENTIALS} -d ${DATA} ${URL}"
 #echo $CURL
 echo ${CURL} | bash
+
+
+#!/bin/bash
+
+
+#CONTAINER=http://0.0.0.0:8080/kie-server/services/rest/server/containers/solar-village
+
+#URL=${CONTAINER}/processes/new-order-permitting-kjar.NewOrderProcess/instances
+#URL=${CONTAINER}/processes/new-order-permitting-kjar.NewOrder/instances
+#DATA="'{\"address\":\"$1\", \"condo\":\"$2\", \"hoaMeetingDate\":\"$3\"}'"
+
+#echo "URL: ${URL}"
+#echo "DATA: ${DATA}"
+
+#CURL="curl -X POST -H \"Accept: application/json\" -H \"Content-Type: application/json\" --user jboss:bpms -d ${DATA} ${URL}"
+
+#echo ${CURL} | bash
