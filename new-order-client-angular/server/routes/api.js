d1d811eb4e0087313f31fdd0ef22601e2cf45240
Ricardo Castañeda <ricardojudo@gmail.com>
2018-02-22 13:52:09 -0600

    First approach of api implementation

--- new-order-client-angular/server/routes/api.js
+++ new-order-client-angular/server/routes/api.js
@@ -1,56 +1,254 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const async = require('async')
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('api works');
+  res.json('api works');
 });
+
+
 
 /* NewOrders */
 
+var buildNewOrders=(data)=>{
+    return [];
+}
+var buildNewOrder=(data)=>{
+    return {};
+}
+
+
 router.get('/newOrders', (req, res) => {
-    var status=req.params.status
-    res.send([]);
+    var status=req.query['status']
+
+    //console.log(req.query)
+    //${KIE_SERVER_HOST}/kie-server/services/rest/server/queries/processes/${PROCESS_DEFINITION}/instances?status=$1
+    const kieServerHost = req.headers['kieserverhost']
+    const authorization = req.headers['authorization']
+    const url=`${kieServerHost}/kie-server/services/rest/server/queries/processes/new-order-permitting-kjar.NewOrderProcess/instances?status=${status}`
+
+    //console.log("<<<<"+ url)
+
+    let headers = {
+        'Authorization': authorization};
+    axios.get(url, {headers: headers}).then((response) => {
+        var newOrders = buildNewOrders(response.data)
+        res.json(newOrders);
+    }).catch((error) => {
+        console.error(error);
+        res.status(422).json([]);
+    });
+    //console.log(req.headers)
+    
 });
 
 router.get('/newOrders/:id', (req, res) => {
-    //req.params.id
-    res.send({});
+    const id=req.params['id']
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+    const url=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/processes/instances/${id}`
+    
+    console.log("<<<<"+ url)
+
+    let headers = {
+        'Authorization': authorization};
+    axios.get(url, {headers: headers}).then((response) => {
+        var newOrder = buildNewOrder(response.data)
+        res.json(newOrder);
+    }).catch((error)=>{ 
+        console.error(error);
+        res.status(422).json([]);
+    });
 });
 
 router.delete('/newOrders/:id', (req, res) => {
-    res.send({});
+    const id=req.params['id']
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+    const url=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/processes/instances/${id}`
+        
+    let headers = {
+        'Authorization': authorization};
+    axios.delete(url, {headers: headers}).then((response) => {
+        var newOrder = buildNewOrder(response.data)
+        res.json();
+    }).catch((error)=>{ 
+        console.error(error);
+        res.status(422).json([]);
+    });
 });
 
 router.post('/newOrders', (req, res) => {
-    res.send({});
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+    let url=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/processes/new-order-permitting-kjar.NewOrderProcess/instances`
+    
+    let params = {
+        'address':req.params['address'],
+        'condo': req.params['condo'],
+        'hoaMeetingDate': req.params['hoaMeetingDate']
+    }
+
+
+    axios.post(url,params, {headers: headers}).then((response) => {        
+        res.json(respose);
+    }).catch((error)=>{ 
+        console.error(error);
+        res.status(422).json([]);
+    });
 });
 
 
 
 /*HOA Meetings */
 
+var buildHoaMeetings = (data) => {
+    return [];
+}
+
+var buildHoaMeeting = (data) => {
+    return {};
+}
+
 router.get('/hoaMeetings', (req, res) => {
-    res.send([]);
+    const kieServerHost = req.headers['kieserverhost']
+    const authorization = req.headers['authorization']
+    
+    const url=`URL=${kieServerHost}/kie-server/services/rest/server/queries/tasks/instances/owners`
+    console.log("<<<<"+ url)
+
+    let headers = {
+        'Authorization': authorization};
+    axios.get(url, {headers: headers}).then((response) => {
+        var hoaMeetings = buildHoaMeetings(response.data)
+        res.json(hoaMeetings);
+    }).catch((error)=>{ 
+        console.error(error);
+        res.status(422).json([]);
+    });
 });
 
 router.get('/hoaMeetings/potential', (req, res) => {
-    res.send([]);
+    const groups=req.query['groups']
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+    const url=`${kieServerHost}/kie-server/services/rest/server/queries/tasks/instances/pot-owners?groups=${groups}`    
+
+    console.log("<<<<"+ url)
+
+    let headers = {
+        'Authorization': authorization};
+    axios.get(url, {headers: headers}).then((response) => {
+        var hoaMeetings = buildHoaMeetings(response.data)
+        res.json(hoaMeetings);
+    }).catch((error)=>{ 
+        console.error(error);
+        res.status(422).json([]);
+    });
 });
 
 router.get('/hoaMeetings/:id', (req, res) => {
-    res.send({});
+    const id=req.params['id']
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+
+    let headers = {
+        'Authorization': authorization};
+
+    let url=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/tasks/${id}`
+    let urlVars=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/tasks/${id}/contents/input`    
+
+    let getDetails=(callback)=>{
+        axios.get(url, {headers: headers}).then((response) => {
+           callback(null, response)
+        }).catch((error)=>{ 
+            callback(error,null)
+        });
+    }
+
+    let getVars=(callback)=>{
+        axios.get(url, {headers: headers}).then((response) => {
+            callback(null, response)
+        }).catch((error)=>{ 
+           callback(error,null)
+        });
+    }
+
+    async.parallel([getDetails,getVars], (err, results)=>{
+        if(err){
+            console.log(err)
+            res.status(422).json({})
+            return;
+        }
+
+        console.log(results)
+        res.json({});
+    });    
 });
 
 router.put('/hoaMeetings/:id/claimed', (req, res) => {
-    res.send({});
+    const taskId = req.params['id']
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+    let headers = {
+        'Authorization': authorization};
+
+    let urlClaim=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/tasks/${taskId}/states/claimed`
+    let urlStart=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/tasks/${taskId}/states/started
+    `
+
+    var claim = (callback)=>{
+        axios.put(urlClaim,{}, {headers: headers}).then((response) => {        
+            callback(null, response);
+        }).catch((error)=>{ 
+            callback(error,null)
+        });
+    };
+
+    var start = (callback)=>{
+        axios.put(urlStart,{}, {headers: headers}).then((response) => {        
+            callback(null, response);
+        }).catch((error)=>{ 
+            callback(error,null)
+        });
+    };
+
+    async.series([claim, start],(err,results) => {
+        if(err){
+            res.status(422).json({});
+            return;
+        }
+
+        res.json({});
+    })
+   
 });
 
 router.put('/hoaMeetings/:id/closed', (req, res) => {
-    
+    const taskId = req.params['id']
+    const kieServerHost = req.headers['kieserverhost']
+    const kieContainerName = req.headers['kiecontainername']
+    const authorization = req.headers['authorization']
+    let headers = {
+        'Authorization': authorization};
+
+    let url=`${kieServerHost}/kie-server/services/rest/server/containers/${kieContainerName}/tasks/${taskId}/states/claimed`
+   
     var data = {'approved':req.params.approved}    
-    res.send({});
+
+    axios.put(url, data, {headers: headers}).then((response) => {
+        req.json({});
+    }).catch((error)=>{
+        req.status(422).json({})
+    })
 });
 
 
--- new-order-client-angular/src/app/interceptors/auth-interceptor.ts
+++ new-order-client-angular/src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
+import { HttpRequest, HttpResponse, HttpHeaders, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
 
 import { Observable } from "rxjs/Observable";
 import { User } from "../models/user";
@@ -11,12 +11,15 @@ export class AuthInterceptor implements HttpInterceptor{
     constructor(private userService: UserService){}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-        let auth = this.userService.getBasicAuthValue()
-        //console.log(">>>> "+ auth)
-        let _request = request.clone({
-            setHeaders: {'Authorization': auth}
+        let auth = this.userService.getBasicAuthValue();
+        let user = this.userService.getCurrentUser();
+        const headers = new HttpHeaders({
+            'Authorization': auth, 
+            'KieServerHost': user.kieServerHost,
+            'KieContainerName': user.kieContainerName
         });
 
+        const _request = request.clone({headers});
         return next.handle(_request);
     }
 
